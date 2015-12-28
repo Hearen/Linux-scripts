8ff92a719ef391dd2b49b35f0dbf1bde084d684a
LHearen <lhearen@126.com>
2015-12-28 15:07:39 +0800

    new scripts added

---
+++ tools/resDir.sh
@@ -0,0 +1,13 @@
+#-------------------------------------------
+# Author      : LHearen
+# E-mail      : LHearen@126.com
+# Time        : 2015-12-28 14:48
+# Description : Used to prepared different directories in remote
+#               testing clients;
+#-------------------------------------------
+
+mkdir -p /home/res
+cd /home/res
+mkdir images
+mkdir iso
+mkdir xml
---
+++ tools/shadow.sh
@@ -0,0 +1,8 @@
+#-------------------------------------------
+# Author      : LHearen
+# E-mail      : LHearen@126.com
+# Time        : 2015-12-28 13:32
+# Description : Used to access Google and other blocked sites in RPC.China
+#-------------------------------------------
+
+sslocal -c /etc/shadowsocks.json  1>&2 2>/dev/null & 

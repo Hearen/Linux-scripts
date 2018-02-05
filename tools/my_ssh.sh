c1afb3db07a47dabfc3f0e0099eb56cd7c4dfb71
LHearen <LHearen@gmail.com>
2018-02-05 18:17:45 +0800

    Update and rename my_ssh to my_ssh.sh

--- tools/my_ssh.sh
+++ tools/my_ssh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set ip [lindex $argv 0];
-spawn ssh root@133.133.135.$ip
+spawn ssh root@133.133.135.${ip}
 expect "password:" { send "onceas\r"}
 expect "#" {send "cd /home/lhearen/RESTful-libvirt \r"}
 interact
